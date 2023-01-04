32454435f90c3e5a2982b8135abc205f121fe3f8
Iury189 <iury.fernandes@protonmail.com>
2023-01-04 18:40:56 -0300

    Improving fields of foreign keys

--- resources/views/dojo/create.blade.php
+++ resources/views/dojo/create.blade.php
@@ -23,20 +23,28 @@
                         <div>
                             <x-input-label for="fighter_id" :value="__('Fighter:')" />
                             <x-select id="fighter_id" class="block w-full" name="fighter_id" autofocus>
-                                <option {{ old('fighter_id') == '' ? 'selected' : '' }} value="">{{ __('Escolha o fighter') }}</option>
-                                @foreach($fighter as $f)
-                                    <option {{ old('fighter_id') == $f->id ? 'selected' : '' }} value="{{ $f->id }}">{{ $f->nome }}</option>
-                                @endforeach
+                                @if ($fighter->isEmpty())
+                                    <option>{{ __('Sem registros de Fighters') }}</option>
+                                @else
+                                    <option {{ old('fighter_id') == '' ? 'selected' : '' }} value="">{{ __('Escolha o fighter') }}</option>
+                                        @foreach($fighter as $f)
+                                            <option {{ old('fighter_id') == $f->id ? 'selected' : '' }} value="{{ $f->id }}">{{ $f->nome }}</option>
+                                        @endforeach
+                                @endif
                             </x-select>
                         </div>
                         <br>
                         <div>
                             <x-input-label for="master_id" :value="__('Master:')" />
                             <x-select id="master_id" class="block w-full" name="master_id" autofocus>
-                                <option {{ old('master_id') == '' ? 'selected' : '' }} value="">{{ __('Escolha o master') }}</option>
-                                @foreach($master as $m)
-                                    <option {{ old('master_id') == $m->id ? 'selected' : '' }} value="{{ $m->id }}">{{ $m->nome }}</option>
-                                @endforeach
+                                @if ($master->isEmpty())
+                                    <option>{{ __('Sem registros de Masters') }}</option>
+                                @else
+                                    <option {{ old('master_id') == '' ? 'selected' : '' }} value="">{{ __('Escolha o master') }}</option>
+                                        @foreach($master as $m)
+                                            <option {{ old('master_id') == $m->id ? 'selected' : '' }} value="{{ $m->id }}">{{ $m->nome }}</option>
+                                        @endforeach
+                                @endif
                             </x-select>
                         </div>
                         <br>
--- resources/views/dojo/update.blade.php
+++ resources/views/dojo/update.blade.php
@@ -6,7 +6,7 @@
             {{ __("Atualizar dojô Nº $dojo->id") }}
         </h2>
     </x-slot>
-    <div class="py-12">   
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
@@ -23,25 +23,33 @@
                         <div>
                             <x-input-label for="fighter_id" :value="__('Fighter:')" />
                             <x-select id="fighter_id" class="block w-full" name="fighter_id" autofocus>
-                                <option {{ $dojo->fighter_id == '' ? 'selected' : '' }} value="">{{ __('Escolha o fighter') }}</option>
-                                @foreach($fighter as $f)
-                                    <option {{ $dojo->fighter_id == $f->id ? 'selected' : '' }} value="{{ $f->id }}">{{ $f->nome }}</option>
-                                @endforeach
+                                @if ($fighter->isEmpty())
+                                    <option>{{ __('Sem registros de Figters') }}</option>
+                                @else
+                                    <option {{ $dojo->fighter_id == '' ? 'selected' : '' }} value="">{{ __('Escolha o fighter') }}</option>
+                                        @foreach($fighter as $f)
+                                            <option {{ $dojo->fighter_id == $f->id ? 'selected' : '' }} value="{{ $f->id }}">{{ $f->nome }}</option>
+                                        @endforeach
+                                @endif
                             </x-select>
                         </div>
                         <br>
                         <div>
                             <x-input-label for="master_id" :value="__('Master:')" />
                             <x-select id="master_id" class="block w-full" name="master_id" autofocus>
-                                <option {{ $dojo->master_id == '' ? 'selected' : '' }} value="">{{ __('Escolha o master') }}</option>
-                                @foreach($master as $m)
-                                    <option {{ $dojo->master_id == $m->id ? 'selected' : '' }} value="{{ $m->id }}">{{ $m->nome }}</option>
-                                @endforeach
+                                @if ($master->isEmpty())
+                                    <option>{{ __('Sem registros de Masters') }}</option>
+                                @else
+                                    <option {{ $dojo->master_id == '' ? 'selected' : '' }} value="">{{ __('Escolha o master') }}</option>
+                                        @foreach($master as $m)
+                                            <option {{ $dojo->master_id == $m->id ? 'selected' : '' }} value="{{ $m->id }}">{{ $m->nome }}</option>
+                                        @endforeach
+                                @endif
                             </x-select>
                         </div>
                         <br>
                         <div>
-                            <x-primary-button class="ml-3"><i class="fa fa-arrows-rotate"></i>&nbsp; {{ __('Atualizar') }} </x-primary-button> 
+                            <x-primary-button class="ml-3"><i class="fa fa-arrows-rotate"></i>&nbsp; {{ __('Atualizar') }} </x-primary-button>
                         </div>
                     </form>
                 </div>
